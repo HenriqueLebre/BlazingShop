1e2aae9360b05d19bfb8f03e4b52a7a484d7b20f
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-21 11:05:51 -0300

    incluindo EF c data annotation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,17 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using BlazingShop.Models;
+
 namespace BlazingShop.Category;
 
 public class Category{
 
-    public Category()
-    {
-        
-    }
-
-    public Category(int id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
+    [Key]
+    [Required(ErrorMessage = "Id é obrigatório!")]
     public int Id { get; set; }
-    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Titulo é obrigatório")]
+    [MaxLength(50, ErrorMessage = "Título deve ter no máximo 50 caracteres.")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres.")]
+    public string Title { get; set; } = string.Empty;
+
+    public List<Product> Products { get; set; } = new();
+
 }
--- Models/Products.cs
+++ Models/Products.cs
@@ -1,28 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace BlazingShop.Models;
 
-public class Product{
+public class Product
+{
+    [Key]
+    [Required(ErrorMessage = "Id é obrigatório")]
+    public int Id { get; set; }
 
-    public Product()
-    {
-        
-    }
+    [Required(ErrorMessage = "Título é obrigatório")]
+    [MaxLength(150, ErrorMessage = "Título deve ter no máximo 150 caracteres")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
 
-    public Product(int id, string name, string description, string image, double price, int categoryId, Category category)
-    {
-        Id = id;
-        Name = name;
-        Description =  description;
-        Image = image;
-        Price = price;
-        CategoryId = categoryId;
-        Category = category;
-    }
+    [Required(ErrorMessage = "Preço é obrigatório")]
+    [DataType(DataType.Currency)]
+    [Range(0, 9999, ErrorMessage = "Preço deve estar entre 0 e 9999")]
+    public decimal Price { get; set; }
 
-    public int Id {get; set;}
-    public string Name {get; set;}
-    public string Description {get; set;}
-    public string Image {get; set;}
-    public double Price {get; set;}
-    public int CategoryId {get; set;}
-    public Category Category {get; set;} = new();
+    [Required(ErrorMessage = "Categoria é obrigatória")]
+    [Range(1, 9999, ErrorMessage = "Categoria deve estar entre 1 e 9999")]
+    public int CategoryId { get; set; }
+
+    public Category Cate { get; set; } = null!;
 }
