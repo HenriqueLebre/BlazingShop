8c503236419d3ea74eab40a3b8cc62ef36b15a6a
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-22 17:01:40 -0300

    criando pag de novos produtos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 
-using BlazingShop.Data;
+// using BlazingShop.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
